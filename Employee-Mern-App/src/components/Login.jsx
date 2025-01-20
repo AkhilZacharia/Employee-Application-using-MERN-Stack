f83243375d20a1eae4f47d1760442c0247101ac1
AkhilZacharia <akhilzacharia35@gmail.com>
2025-01-21 03:05:55 +0530

    first commit

--- Employee-Mern-App/src/components/Login.jsx
+++ Employee-Mern-App/src/components/Login.jsx
@@ -5,12 +5,39 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Login = () => {
-
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
   const[form,setForm]=useState({
     Email:'',
     Password:''
   }) 
   const navigate=useNavigate();
+
+  function validate()   {
+    let isValid = true;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(form.Email)) {
+      setEmailError('Please enter a valid email.');
+      isValid = false;
+    } else {
+      setEmailError('');
+    }
+    const passwordRegex = /^(?=.*\d).{5,}$/;
+    if (!passwordRegex.test(form.Password)) {
+      setPasswordError('Password must be at least 5 characters long and contain at least one number.');
+      isValid = false;
+    } else {
+      setPasswordError('');
+    }
+    return isValid;
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      loginValue()
+    }
+  };
 
   function loginValue(){
     // console.log(form);
@@ -41,11 +68,11 @@ const Login = () => {
           <Box component="form"  sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}> 
             <TextField label="Email" name="Email" type="email"  required onChange={(e)=>{
           setForm({...form,Email:e.target.value})                     
-        }}/> 
+        }}/> <p style={{color:'maroon'}}>{emailError}</p>
             <TextField label="Password" name="Password" type="password"   required onChange={(e)=>{
-          setForm({...form,Password:e.target.value})                     
-        }} /> 
-            <Button variant="contained" color="primary" onClick={loginValue}> Login </Button> 
+          setForm({...form,Password:e.target.value})                    
+        }} /><p style={{color:'maroon'}}>{passwordError}</p> 
+            <Button variant="contained" color="primary" onClick={login}> Login </Button> 
           </Box> 
           <Link to={'/register'} style={{textDecoration:'none'}}>New user? please Register here</Link> 
         </Box> 
