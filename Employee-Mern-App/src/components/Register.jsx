cdb414d79d0528a41c116b502e41f48b214dd0b0
AkhilZacharia <akhilzacharia35@gmail.com>
2025-01-22 01:52:21 +0530

    validation

--- Employee-Mern-App/src/components/Register.jsx
+++ Employee-Mern-App/src/components/Register.jsx
@@ -12,7 +12,52 @@ const Register = () => {
     Password:'',
     Phone: 0
   }) 
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [phoneError, setPhoneError] = useState('');
+  
   const navigate=useNavigate();
+
+  function validate()   {
+    let isValid = true;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(form.Email)) {
+      setEmailError('Please enter a valid email.');
+      isValid = false;
+    } else {
+      setEmailError('');
+    }
+    const passwordRegex = /^(?=.*\d).{5,}$/;
+    if (!passwordRegex.test(form.Password)) {
+      setPasswordError('Password must be at least 5 characters long and contain at least one number.');
+      isValid = false;
+    } else {
+      setPasswordError('');
+    }
+    const nameRegex = /^[A-Z][a-zA-Z]{1,}$/;
+    if (!nameRegex.test(form.Name)) {
+      setNameError('Name must start with a capital letter and be at least 2 characters long');
+      isValid = false;
+    } else {
+      setNameError('');
+    }
+    const phoneRegex = /^(?=.*\d).{6,}$/;
+    if (!phoneRegex.test(form.Phone)) {
+      setPhoneError('Phone no must be at least 6 digits.');
+      isValid = false;
+    } else {
+      setPhoneError('');
+    }
+    return isValid;
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      registerValue()
+    }
+  };
 
   function registerValue(){
     console.log(form);
@@ -30,22 +75,23 @@ const Register = () => {
         <Typography variant="h4" component="h1" gutterBottom> Register User </Typography> 
         <Box component="form"  sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}> 
           <Grid2 container spacing={2}>
-            <Grid2 item xs={12} sm={6}> <TextField label="Name" name="Name"  required fullWidth onChange={(e)=>{
+            <Grid2 item xs={12} sm={6}> <TextField label="Name" name="Name" helperText={nameError} required fullWidth onChange={(e)=>{
           setForm({...form,Name:e.target.value}) }}/> </Grid2> 
-            <Grid2 item xs={12} sm={6}> <TextField label="Age" name="Age" type="number"  required fullWidth onChange={(e)=>{
+            <Grid2 item xs={12} sm={6}> <TextField label="Age" name="Age" type="number" required fullWidth onChange={(e)=>{
           setForm({...form,Age:e.target.value}) }}/> </Grid2> 
-            <Grid2 item xs={12} sm={6}> <TextField label="Email" name="Email" type="email"  required fullWidth onChange={(e)=>{
+            <Grid2 item xs={12} sm={6}> <TextField label="Email" name="Email" type="email" helperText={emailError} required fullWidth onChange={(e)=>{
           setForm({...form,Email:e.target.value}) }}/> </Grid2> 
-            <Grid2 item xs={12} sm={6}> <TextField label="Password" name="Password" type="password"  required fullWidth onChange={(e)=>{
+            <Grid2 item xs={12} sm={6}> <TextField label="Password" name="Password" type="password" helperText={passwordError} required fullWidth onChange={(e)=>{
           setForm({...form,Password:e.target.value}) }}/> </Grid2>  
-            <Grid2 item xs={12} sm={6}> <TextField label="Phone" name="Phone" type="number"  required fullWidth onChange={(e)=>{
-          setForm({...form,Phone:e.target.value}) }}/> </Grid2> 
+            <Grid2 item xs={12} sm={6}> <TextField label="Phone" name="Phone" type="number" helperText={phoneError} required fullWidth onChange={(e)=>{
+          setForm({...form,Phone:e.target.value}) }}/>
+            </Grid2> 
 
             </Grid2> 
         </Box> 
       </CardContent> 
       <CardActions className="form-actions"> 
-      <Button variant="contained" color="success" type="submit" onClick={registerValue} fullWidth> Submit </Button> 
+      <Button variant="contained" color="success" type="submit" onClick={register} fullWidth> Submit </Button> 
       </CardActions> 
     </Card> 
   </Container> 
